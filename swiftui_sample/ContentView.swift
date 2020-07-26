42cb2070b54bdf4cbf70e780e76be8dfab98c935
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-07-26 20:32:34 +0900

    add text option

--- swiftui_sample/ContentView.swift
+++ swiftui_sample/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello World")
+            .font(.largeTitle)
+            .foregroundColor(Color.red)
     }
 }
 
