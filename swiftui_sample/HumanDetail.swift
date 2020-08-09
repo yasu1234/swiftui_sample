9a2af26318501f71383580bc5f00a32292ff2101
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-08-09 11:56:36 +0900

    add human detail view

---
+++ swiftui_sample/HumanDetail.swift
@@ -0,0 +1,21 @@
+//
+//  HumanDetail.swift
+//  swiftui_sample
+//
+//  Created by 神代泰宏 on 2020/08/09.
+//  Copyright © 2020 神代泰宏. All rights reserved.
+//
+
+import SwiftUI
+
+struct HumanDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HumanDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        HumanDetail()
+    }
+}
