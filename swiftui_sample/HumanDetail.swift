15b496bf7fe17d1e1be3db9af6d311db67796903
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-12-06 21:30:39 +0900

    add alert

--- swiftui_sample/HumanDetail.swift
+++ swiftui_sample/HumanDetail.swift
@@ -10,11 +10,31 @@ import SwiftUI
 
 struct HumanDetail: View {
     var human: Human
+    @State private var isShowOkAlert: Bool = false
+    @Environment(\.presentationMode) var presentation: Binding<PresentationMode>
+    
     var body: some View {
         VStack {
             Text("名前：" + human.name)
             Text("年齢：" + human.age)
             Text("ID：" + String(human.id))
+            
+            Button(action: {
+                self.isShowOkAlert = true
+                
+            }) {
+            Text("戻る")
+            }
+            .alert(isPresented: $isShowOkAlert) { () -> Alert in
+                let alert: Alert =
+                Alert(title: Text("前の画面に戻る"),
+                      message: Text("前の画面に戻りますか？"),
+                      primaryButton: .default(Text("OK"),
+                        action: {
+                            self.presentation.wrappedValue.dismiss()
+                        }), secondaryButton: .cancel())
+                return alert
+            }
         }
     }
 }
